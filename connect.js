e6ae6f9b6509cc804f8ccc376f577794189dfe71
Sahil <jaglan89010@gmail.com>
2026-09-20 21:14:33 +0530

    Fix production deployment configuration

--- connect.js
+++ connect.js
@@ -1,9 +1,13 @@
 const mongoose=require('mongoose')
 
 async function connectDB(url) {
-    console.log(url);
-    
-    mongoose.connect(url)
+     try {
+    await mongoose.connect(url);
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+    process.exit(1);
+  }
 }
 
 module.exports={connectDB}
--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express= require('express')
-const PORT= 8001
+const PORT = process.env.PORT || 8001;
 const app=express();
+// const DbUrl='mongodb+srv://jaglan89010_db_user:HagQ4BLpbBKqLPbe@urlshortener.xkganbb.mongodb.net/?appName=UrlShortener'
+const MONGODB_URI = process.env.MONGODB_URI;
+if (!MONGODB_URI) {
+  console.error("MONGODB_URI is not configured");
+  process.exit(1);
+}
 
 const {connectDB}= require('./connect')
 const router=require('./routes/url')
 app.use(express.json()) 
-connectDB('mongodb://localhost:27017/short-url')
+connectDB(MONGODB_URI)
+// connectDB('mongodb://localhost:27017/short-url')
 app.use('/url',router)
 
 
 
-app.listen(PORT,()=>console.log('Server Started'));
+app.listen(PORT,"0.0.0.0",()=>console.log('Server Started'));
 
